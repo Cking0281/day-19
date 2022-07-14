912646381f9588859b38d543b1a490776eef8387
Cody King <cody.king04@outlook.com>
2022-07-14 15:50:03 -0400

    Made etch-a-sketch game

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from turtle import Turtle, Screen
+
+
 def main():
-    pass
+    turtle = Turtle()
+
+    def move_forward():
+        turtle.forward(10)
+
+    def move_backward():
+        turtle.back(10)
+
+    def move_counter_clockwise():
+        turtle.left(10)
+
+    def move_clockwise():
+        turtle.right(10)
+
+    def clear_drawing():
+        turtle.reset()
+
+    screen = Screen()
+    screen.listen()
+
+    screen.onkeypress(move_forward, "w")
+    screen.onkeypress(move_backward, "s")
+    screen.onkeypress(move_counter_clockwise, "a")
+    screen.onkeypress(move_clockwise, "d")
+    screen.onkeypress(clear_drawing, "c")
+
+    screen.exitonclick()
 
 
 if __name__ == "__main__":
