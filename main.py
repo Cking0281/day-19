8b134ea4b007b2ba8f7a92bb27e307617f69c495
Cody King <cody.king04@outlook.com>
2022-07-19 09:53:26 -0400

    Added check if user guess right or not and ends the program once the race is over.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
+import random
 
 
 def main():
+    is_race_on = False
     screen = Screen()
     screen.setup(width=500, height=400)
     user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
@@ -9,16 +11,30 @@ def main():
 
     race_position = -100
 
-    turtles = {}
+    turtles = []
 
     for color in colors:
-        turtles[color] = (Turtle(shape="turtle"))
-        turtles[color].color(color)
-        turtles[color].penup()
-        turtles[color].goto(x=-230, y=race_position)
+        new_turtle = (Turtle(shape="turtle"))
+        new_turtle.color(color)
+        new_turtle.penup()
+        new_turtle.goto(x=-230, y=race_position)
+        turtles.append(new_turtle)
         race_position += 40
 
-    screen.exitonclick()
+    if user_bet:
+        is_race_on = True
+
+    while is_race_on:
+        for turtle in turtles:
+            if turtle.xcor() > 230:
+                is_race_on = False
+                winning_color = turtle.color()[0]
+                if winning_color != user_bet:
+                    print(f"{winning_color.capitalize()} won the race. You lost.")
+                elif winning_color == user_bet:
+                    print(f"{winning_color.capitalize()} won the race. You won.")
+            random_distance = random.randint(0, 10)
+            turtle.forward(random_distance)
 
 
 if __name__ == "__main__":
