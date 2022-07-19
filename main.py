a70996d0c119d143b44443a29511b3e69e787cd0
Cody King <cody.king04@outlook.com>
2022-07-18 20:31:09 -0400

    Added multiple turtles for turtle race

--- main.py
+++ main.py
@@ -2,31 +2,19 @@ from turtle import Turtle, Screen
 
 
 def main():
-    turtle = Turtle()
-
-    def move_forward():
-        turtle.forward(10)
-
-    def move_backward():
-        turtle.back(10)
-
-    def move_counter_clockwise():
-        turtle.left(10)
-
-    def move_clockwise():
-        turtle.right(10)
-
-    def clear_drawing():
-        turtle.reset()
-
     screen = Screen()
-    screen.listen()
+    screen.setup(width=500, height=400)
+    user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+    colors = ("red", "orange", "yellow", "green", "blue", "purple")
 
-    screen.onkeypress(move_forward, "w")
-    screen.onkeypress(move_backward, "s")
-    screen.onkeypress(move_counter_clockwise, "a")
-    screen.onkeypress(move_clockwise, "d")
-    screen.onkeypress(clear_drawing, "c")
+    starting_position = -100
+
+    # TODO Need to fix loop variable name
+    for color in colors:
+        color = Turtle(shape="turtle")
+        color.penup()
+        color.goto(x=-230, y=starting_position)
+        starting_position += 40
 
     screen.exitonclick()
 
